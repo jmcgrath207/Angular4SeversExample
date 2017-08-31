83b1bcf78430749dafc5284eb53b91bf2007fabf
sonance207 <sonance207@gmail.com>
2017-08-31 15:24:40 -0500

    working DOM access with templates

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,11 +6,13 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
+
   /*allows variables to be grabbed by other components Child--> Parent */
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpcreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 /*  newServerName = '';*/
-  newServerContent = '';
+/*  newServerContent = '';*/
+@ViewChild('serverControlInput') serverContentInput: ElementRef;
 
   constructor() { }
 
@@ -18,16 +20,17 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer(serverNameInput) {
+    console.log(this.serverContentInput);
     this.serverCreated.emit({
       serverName: serverNameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value
   });
   }
 
   onAddBlueprint(serverNameInput) {
   this.blueprintCreated.emit({
     serverName: serverNameInput.value,
-    serverContent: this.newServerContent,
+    serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
