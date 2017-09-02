c755bbf036175d54077c34391620f66883aeb5a3
sonance207 <sonance207@gmail.com>
2017-09-02 13:01:18 -0500

    Ng lifecyclesn complete

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,9 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit,
+  SimpleChanges,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,9 +11,17 @@ import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation} f
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy{
   /*allow other components to grab variable Parent --> Child*/
   @Input('SrvElement') element: {type: string, name: string, content: string};
+  @Input() name;
 
   constructor() {
     console.log('Constructor Called');
@@ -21,6 +34,28 @@ export class ServerElementComponent implements OnInit, OnChanges {
 
   ngOnInit() {
     console.log('ngOnInit Called');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck Called');
+}
+
+  ngAfterContentInit() {
+  console.log('ngAfterContent Called');
+  }
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked Called');
+  }
+
+  ngAfterViewInit() {
+    console.log('AfterViewInit Called');
+  }
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked Called');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy Called');
   }
 
 }
